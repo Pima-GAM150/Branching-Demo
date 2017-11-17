3214a7633130f391c2e5cbab5e3e2e5446edd655
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-11-16 18:11:27 -0700

    Scripts!

---
+++ Assets/Scripts/DestroyPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayer : MonoBehaviour {
+
+    int destructionTimer;
+
+	// Use this for initialization
+	void Start () {
+        destructionTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (destructionTimer < 120)
+        {
+            destructionTimer += 1;
+        }
+
+	}
+}
