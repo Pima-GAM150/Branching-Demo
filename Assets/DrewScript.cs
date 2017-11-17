33e1bfc4e3264453b0b1ff60c1c41e3f945bbf41
LAPTOP-UHC40UK6\chand <chosteler@mail.pima.edu>
2017-11-16 18:20:28 -0700

    perfection

--- Assets/DrewScript.cs
+++ Assets/DrewScript.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrewScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Text textToEnhance;
+
+    public void OnClick()
+    {
+        textToEnhance.color = new Color(1.0f,0.5f,0.5f,1.0f); 
+    }
 	
 	// Update is called once per frame
 	void Update () {
